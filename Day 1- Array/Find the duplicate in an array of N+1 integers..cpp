2e6c658eb81e8ce5ef7c18e0fa5ae3e3038dd0dd
Ayush Gupta <721ayush@gmail.com>
2022-09-20 16:08:59 +0530

    Day1_Array_Ques

---
+++ Day 1- Array/Find the duplicate in an array of N+1 integers..cpp
@@ -0,0 +1,28 @@
+Optimal Solution -->  Using Linked List Cycle Method- Tortoquise 
+
+Example 1: 
+Input: arr=[1,3,4,2,2]
+Output: 2
+
+Explanation: Since 2 is the duplicate number the answer will be 2.
+
+Example 2:
+
+Input: [3,1,3,4,2]
+
+Output:3
+  
+int findDuplicate(vector < int > & nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
