a6de65c070fa0dbe4779b561e342add6bcfd0d5d
Ayush Gupta <721ayush@gmail.com>
2022-09-27 13:02:35 +0530

    Day4_Array_Ques2

--- Day-4 Array/Longest Subarray sum with 0.cpp
+++ Day-4 Array/Longest Subarray sum with 0.cpp
@@ -1,1 +1,32 @@
 
+Approach - 1 - using two loops - T.C - O(N^2), S.C - O(1)
+  
+Approach 2 - Optimal  - T.C - O(N) , S.C - O(N)
+  // using hashmaps
+  
+  int maxLen(vector<int>nums)
+{
+  int sum = 0;
+  int maxlen = 0;
+  
+  unordered_map<int,int>hash;
+  
+  for(int i = 0;i<nums.size();i++;) {
+    sum += arr[i];
+    
+    if(nums[i] == 0 && maxlen == 0)
+      maxlen = 1;
+    if(sum ==0)
+      maxlen = i + 1;
+    
+    if(hash.find(sum) != hash.end()) 
+    {
+      maxlen = max(maxlen, i - hash[sum]);
+    }
+    else {
+      hash[sum] = i;
+    }
+
+  }  
+  return maxlen;
+}
