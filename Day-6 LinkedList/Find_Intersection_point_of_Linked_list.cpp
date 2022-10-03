716cd648b718f7d35a2db75859f53ddd2de3ac9f
Ayush Gupta <721ayush@gmail.com>
2022-10-03 11:08:01 +0530

    Day6_LinkedList_Ques1

---
+++ Day-6 LinkedList/Find_Intersection_point_of_Linked_list.cpp
@@ -0,0 +1,51 @@
+Approach 1 - T.C - O(N * M), S.C - O(1) - Brute Force Approach
+
+//code
+
+node* intersectionPresent(node* head1,node* head2) {
+    while(head2 != NULL) {
+        node* temp = head1;
+        while(temp != NULL) {
+            //if both nodes are same
+            if(temp == head2) return head2;
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    //intersection is not present between the lists return null
+    return NULL;
+}
+
+Approach 2 - T.C - O(N + M) - HAshing  - S.C - (N)
+  
+  //code
+  node* intersectionPresent(node* head1,node* head2) {
+     unordered_set<node*> st;
+    while(head1 != NULL) {
+       st.insert(head1);
+       head1 = head1->next;
+    }
+    while(head2 != NULL) {
+        if(st.find(head2) != st.end()) return head2;
+        head2 = head2->next;
+    }
+    return NULL;
+
+}
+
+Approach - 3 - Optimal -  O(2*max(length of list1,length of list2)) - S.C - O(1)
+  
+  // code
+  node* intersectionPresent(node* head1,node* head2) {
+    node* d1 = head1;
+    node* d2 = head2;
+    
+    while(d1 != d2) {
+        d1 = d1 == NULL? head2:d1->next;
+        d2 = d2 == NULL? head1:d2->next;
+    }
+    
+    return d1;
+}
+
+  
