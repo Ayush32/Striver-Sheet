2b1f6d77a96e3a590f36f65f26a8134b8842c421
Ayush Gupta <721ayush@gmail.com>
2022-10-21 23:17:08 +0530

    Day11_Stack_Ques7

---
+++ Day-10 Stack/stock_span_problem.cpp
@@ -0,0 +1,19 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+    
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int day = 1;
+        while(!st.empty() && st.top().first <= price){
+            day += st.top().second;
+            st.pop();
+        }
+        st.push({price,day});
+         return day;
+    }
+   
+};
