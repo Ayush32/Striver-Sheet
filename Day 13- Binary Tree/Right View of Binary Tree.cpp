9e2794ba1cd48fb730f5b8ebbf376778d6bad1b8
Ayush Gupta <721ayush@gmail.com>
2022-11-05 12:14:02 +0530

    Day13_Tree_Ques19

---
+++ Day 13- Binary Tree/Right View of Binary Tree.cpp
@@ -0,0 +1,47 @@
+Iterative way - 
+  
+  class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if(root==NULL) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *temp = q.front();
+            res.push_back(temp->val);
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode *curr = q.front();
+                q.pop();
+                if(curr->right!=NULL) q.push(curr->right);
+                if(curr->left!=NULL) q.push(curr->left);
+            }
+        }
+        return res;
+    }
+};
+
+Recursive Way
+ 
+ class Solution {
+    void recursion(TreeNode* root,int level,vector<int>&ans)
+    {
+        if(root == NULL)
+            return;
+        if(level == ans.size())
+        {
+            ans.push_back(root->val);
+        }
+        recursion(root->right,level+1,ans);
+        recursion(root->left,level+1,ans);
+
+    }
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        int level = 0;
+        recursion(root,0,ans);
+        return ans;
+    }
+};
