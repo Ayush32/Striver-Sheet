2deb1f01dabb100864f6c46a8bb0bf838ad1cb82
Ayush Gupta <721ayush@gmail.com>
2023-05-18 13:58:25 +0530

    Create Find_the_Diffrence.cpp
    New Code

---
+++ Find_the_Diffrence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n1 = s.length();
+    int n2 = t.length();
+    // sort(s.begin(),s.end());
+    // sort(t.begin(),t.end());
+    
+    
+    int freqt[128] = {0};
+    // char ans = ' ';
+    for(int i = 0;i<n2;i++)
+    {
+        freqt[t[i] - '0']++;
+    }
+    for(int i = 0;i<n1;i++)
+    {
+        freqt[s[i] - '0']--;
+    }
+    char ans = ' ';
+    for(int i = 0;i<128;i++)
+    {
+        if(freqt[i] == 1){
+            ans = char(i + '0');
+        }
+    }
+return ans;
+    
+    }
+};
