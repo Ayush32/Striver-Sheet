f6f4baabfa6b4cc1a337c2514910540ac00cbc01
Ayush Gupta <721ayush@gmail.com>
2023-05-18 14:48:10 +0530

    Update Find_the_Diffrence.cpp

--- Find_the_Diffrence.cpp
+++ Find_the_Diffrence.cpp
@@ -24,6 +24,24 @@ public:
             ans = char(i + '0');
         }
     }
+        
+     int freqt[26] = {0};
+    // char ans = ' ';
+    for(int i = 0;i<n2;i++)
+    {
+        freqt[t[i] - 'a']++;
+    }
+    for(int i = 0;i<n1;i++)
+    {
+        freqt[s[i] - 'a']--;
+    }
+    char ans = ' ';
+    for(int i = 0;i<=26;i++)
+    {
+        if(freqt[i] == 1){
+            ans = char(i+'a');
+        }
+    }
 return ans;
     
     }
